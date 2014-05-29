51a07acebcf6bcae98893fa7ee211ba8a79e3b1c
zech <zhenjiang.xu@gmail.com>
2014-05-29 17:29:50 -0600

    renamed the file to ox-blog.el
    added testing org file

--- ox-blog.el
+++ ox-blog.el
@@ -1,24 +1,24 @@
-;; ox-jekyll -- is a minor mode, which help to export org-mode file
-;; as an jekyll-formatted html.
+;;; ox-blog -- is a minor mode, which help to export org-mode file
+;;; as an Jekyll- or Hexo-formatted html.
 
 ;; Author: Zech XU
 ;; Created: 2014-05-27
 ;; Version: 1.0
 ;; Package-Requires: ((org "8.0"))
-;; Keywords: org, jekyll
+;; Keywords: org, blog, Jekyll, Hexo
 
 ;; This file is not part of GNU Emacs.
 
 (require 'ox-html)
 
-(defgroup org-export-jekyll nil
-  "Options for exporting Org mode files to jekyll HTML."
-  :tag "Org Jekyll"
+(defgroup org-export-blog nil
+  "Options for exporting Org mode files to blog HTML using Jekyll or Hexo."
+  :tag "Org Blog"
   :group 'org-export
   :version "24.3")
 
 
-(defcustom org-jekyll-include-yaml-front-matter t
+(defcustom org-blog-include-yaml-front-matter t
   "If true, then include yaml-front-matter when exporting to html.
 
 If false, then you should include the yaml front matter like this at the top of the file:
@@ -26,53 +26,64 @@ If false, then you should include the yaml front matter like this at the top of 
 #+BEGIN_HTML
 ---
 layout: post
-title: Org-mode to GitHub pages with Jekyll
-excerpt: Introduce how to use Emacs's Org-mode with Jekyll to generate GitHub Pages
+title: Org-mode to GitHub pages with Jekyll/Hexo
+excerpt: Introduce how to use Emacs's Org-mode to generate GitHub Pages
 categories: [lessons, beginner]
 tags: [Emacs, org-mode, GitHub, Jekyll]
 tagline: \"Supporting tagline\"
 date: 2013-09-15 22:08
 comments: true
 keywords: blog
-description: Instructions on export org file with Jekyll
+description: Instructions on export org file with Jekyll or Hexo
 ---
 #+END_HTML"
-  :group 'org-export-jekyll
+  :group 'org-export-blog
   :type 'boolean)
 
+(defcustom org-blog-engine "hexo"
+  "Default static webpage generator to use.
 
-(defcustom org-jekyll-yaml-layout "post"
-  "Default layout used in Jekyll article."
-  :group 'org-export-jekyll
+This variable will be used to guide the accommodation
+for different generators."
+  :group 'org-export-blog
   :type 'string)
 
-(defcustom org-jekyll-yaml-categories "other"
-  "Default space-separated categories in Jekyll article."
-  :group 'org-export-jekyll
+(defcustom org-blog-yaml-layout "post"
+  "Default layout used in Blog article."
+  :group 'org-export-blog
   :type 'string)
 
-(defcustom org-jekyll-yaml-tags nil
-  "Default space-separated categories in Jekyll article."
-  :group 'org-export-jekyll
+(defcustom org-blog-yaml-categories "other"
+  "Default space-separated categories in Blog article."
+  :group 'org-export-blog
   :type 'string)
 
-(defcustom org-jekyll-yaml-published "true"
-  "Default publish status in Jekyll article."
-  :group 'org-export-jekyll
+(defcustom org-blog-yaml-tags nil
+  "Default space-separated categories in Blog article."
+  :group 'org-export-blog
   :type 'string)
 
-(defcustom org-jekyll-yaml-comments "true"
-  "Default comments (disqus) flag in Jekyll article."
-  :group 'org-export-jekyll
+(defcustom org-blog-yaml-published "true"
+  "Default publish status in Blog article."
+  :group 'org-export-blog
   :type 'string)
 
-(defcustom org-jekyll-path "~/Desktop/blog/rnaer.github.io/_posts"
-  "Default publish dir for Jekyll article."
-  :group 'org-export-jekyll
+(defcustom org-blog-yaml-comments "true"
+  "Default comments (disqus) flag in Blog article."
+  :group 'org-export-blog
   :type 'string)
 
-(defcustom org-jekyll-use-src-plugin nil
-   "If t, org-jekyll exporter eagerly uses plugins instead of
+(defcustom org-blog-path
+  (cond ((string= org-blog-engine "jekyll")
+	 "~/Desktop/blog/rnaer.github.io/_posts")
+	(t
+	 "~/Desktop/blog/hexo-blog/source/_posts"))
+  "Default publish dir for Blog article."
+  :group 'org-export-blog
+  :type 'string)
+
+(defcustom org-blog-use-src-plugin nil
+   "If t, org-blog exporter eagerly uses plugins instead of
 org-mode's original HTML stuff. For example:
 
 #+BEGIN_SRC ruby
@@ -84,39 +95,40 @@ makes:
 {% codeblock ruby %}
 puts \"Hello world\"
 {% endcodeblock %}"
-  :group 'org-export-jekyll-use-src-plugin
+  :group 'org-export-blog-use-src-plugin
   :type 'boolean)
 
 
 ;;; Define Back-End
 
-(org-export-define-derived-backend 'jekyll 'html
-  :export-block '("HTML" "JEKYLL")
+(org-export-define-derived-backend 'blog 'html
+  :export-block '("HTML" "BLOG")
   :menu-entry
-  '(?j "Jekyll: export to HTML with YAML front matter."
-       ((?H "As HTML buffer" org-jekyll-export-as-html)
-        (?h "As HTML file" org-jekyll-export-to-html)))
+  '(?b "Blog: export to HTML with YAML front matter."
+       ((?H "As HTML buffer" org-blog-export-as-html)
+        (?h "As HTML file" org-blog-export-to-html)))
   :translate-alist
-  '((template . org-jekyll-template) ;; add YAML front matter.
-    (src-block . org-jekyll-src-block)
-    (inner-template . org-jekyll-inner-template)) ;; force body-only
+  '((template . org-blog-template) ;; add YAML front matter.
+    (src-block . org-blog-src-block)
+    (inner-template . org-blog-inner-template)) ;; force body-only
   :options-alist
-  '((:jekyll-path "JEKYLL_PATH" nil org-jekyll-path)
-    (:jekyll-layout "JEKYLL_LAYOUT" nil org-jekyll-yaml-layout)
-    (:jekyll-categories "JEKYLL_CATEGORIES" nil org-jekyll-yaml-categories)
-    (:jekyll-tags "JEKYLL_TAGS" nil org-jekyll-yaml-tags)
-    (:jekyll-published "JEKYLL_PUBLISHED" nil org-jekyll-yaml-published)
-    (:jekyll-comments "JEKYLL_COMMENTS" nil org-jekyll-yaml-comments)))
+  '((:blog-engine "BLOG_ENGINE" nil org-blog-engine)
+    (:blog-path "BLOG_PATH" nil org-blog-path)
+    (:blog-layout "BLOG_LAYOUT" nil org-blog-yaml-layout)
+    (:blog-categories "BLOG_CATEGORIES" nil org-blog-yaml-categories)
+    (:blog-tags "BLOG_TAGS" nil org-blog-yaml-tags)
+    (:blog-published "BLOG_PUBLISHED" nil org-blog-yaml-published)
+    (:blog-comments "BLOG_COMMENTS" nil org-blog-yaml-comments)))
 
 
 ;;; Internal Filters
 
-(defun org-jekyll-src-block (src-block contents info)
-  "Transcode SRC-BLOCK element into jekyll code template format
-if `org-jekyll-use-src-plugin` is t. Otherwise, perform as
+(defun org-blog-src-block (src-block contents info)
+  "Transcode SRC-BLOCK element into blog code template format
+if `org-blog-use-src-plugin` is t. Otherwise, perform as
 `org-html-src-block`. CONTENTS holds the contents of the item.
 INFO is a plist used as a communication channel."
-  (if org-jekyll-use-src-plugin
+  (if org-blog-use-src-plugin
       (let ((language (org-element-property :language src-block))
             (value (org-remove-indentation
                     (org-element-property :value src-block))))
@@ -128,17 +140,17 @@ INFO is a plist used as a communication channel."
 
 ;;; Template
 
-(defun org-jekyll-template (contents info)
+(defun org-blog-template (contents info)
   "Return complete document string after HTML conversion.
 CONTENTS is the transcoded contents string. INFO is a plist
 holding export options."
-  (if org-jekyll-include-yaml-front-matter
+  (if org-blog-include-yaml-front-matter
       (concat
-       (org-jekyll--yaml-front-matter info)
+       (org-blog--yaml-front-matter info)
        contents)
     contents))
 
-(defun org-jekyll-inner-template (contents info)
+(defun org-blog-inner-template (contents info)
   "Return body of document string after HTML conversion.
 CONTENTS is the transcoded contents string. INFO is a plist
 holding export options."
@@ -147,7 +159,7 @@ holding export options."
    (let ((depth (plist-get info :with-toc)))
      (when depth (org-html-toc depth info)))
    ;; PREVIEW mark on the top of article.
-   (unless (equal "true" (plist-get info :jekyll-published))
+   (unless (equal "true" (plist-get info :blog-published))
      "<span style=\"background: red;\">PREVIEW</span>")
    ;; Document contents.
    contents
@@ -158,17 +170,17 @@ holding export options."
 
 ;;; YAML Front Matter
 
-(defun org-jekyll--yaml-front-matter (info)
+(defun org-blog--yaml-front-matter (info)
   (let ((title (plist-get info :title))
         (date  (or (plist-get info :date)
 		   (format-time-string "%Y-%m-%d %a %H:%M:%S"
 				       (current-time))))
-        (layout (plist-get info :jekyll-layout))
-        (categories (plist-get info :jekyll-categories))
-        (published  (plist-get info :jekyll-published))
-	(tags (cons (plist-get info :jekyll-tags)
+        (layout (plist-get info :blog-layout))
+        (categories (plist-get info :blog-categories))
+        (published  (plist-get info :blog-published))
+	(tags (cons (plist-get info :blog-tags)
 		    (plist-get info :tag)))
-        (comments (plist-get info :jekyll-comments)))
+        (comments (plist-get info :blog-comments)))
     (concat
      "---"
      "\ntitle: " (org-element-interpret-data title)
@@ -184,16 +196,16 @@ holding export options."
 
 ;;; Filename and Date Helper
 
-(defun org-jekyll-date-from-filename (&optional filename)
+(defun org-blog-date-from-filename (&optional filename)
   "Get the date string from filename FILENAME."
   (let ((fn (file-name-nondirectory (or filename (buffer-file-name)))))
     (if (string-match "^[0-9]+-[0-9]+-[0-9]+" fn)
         (match-string 0 fn)
       nil)))
 
-(defun org-jekyll-property-list (&optional filename)
+(defun org-blog-property-list (&optional filename)
   "Get property list of the current buffer or in the file named FILENAME."
-  (let ((backend 'jekyll) plist)
+  (let ((backend 'blog) plist)
     (if filename
         (with-temp-buffer
           (insert-file-contents filename)
@@ -203,24 +215,24 @@ holding export options."
       (setq plist (org-export-get-environment backend))
       plist)))
 
-(defun org-jekyll-property (keys &optional filename)
+(defun org-blog-property (keys &optional filename)
   "Get the property by the KEYS in the file named FILENAME.
 
-Example: (org-jekyll-property '(:jekyll-layout) \"index.org\")"
-  (let ((plist (org-jekyll-property-list filename)))
-    (mapcar (lambda (key) (org-export-data-with-backend (plist-get plist key) 'jekyll plist))
+Example: (org-blog-property '(:blog-layout) \"index.org\")"
+  (let ((plist (org-blog-property-list filename)))
+    (mapcar (lambda (key) (org-export-data-with-backend (plist-get plist key) 'blog plist))
             keys)))
 
-(defun org-jekyll-date-from-property (&optional filename)
-  (let ((plist (org-jekyll-property-list filename)))
+(defun org-blog-date-from-property (&optional filename)
+  (let ((plist (org-blog-property-list filename)))
     (org-read-date
      nil nil
-     (org-export-data-with-backend (plist-get plist :date) 'jekyll plist))))
+     (org-export-data-with-backend (plist-get plist :date) 'blog plist))))
 
-(defun org-jekyll-create-filename ()
+(defun org-blog-create-filename ()
   "Return the string of current buffer file name by adding or
 replacing the heading date."
-  (let ((date (org-jekyll-date-from-property))
+  (let ((date (org-blog-date-from-property))
         (file (file-name-nondirectory (buffer-file-name)))
         (dir (file-name-directory (buffer-file-name))))
     (expand-file-name
@@ -232,16 +244,16 @@ replacing the heading date."
 ;;; End-User functions
 
 ;;;###autoload
-(defun org-jekyll-export-as-html
+(defun org-blog-export-as-html
   (&optional async subtreep visible-only body-only ext-plist)
   "Export current buffer to a HTML buffer adding some YAML front matter."
   (interactive)
-  (org-export-to-buffer 'jekyll "*Org Jekyll HTML Export*"
+  (org-export-to-buffer 'blog "*Org Blog HTML Export*"
     async subtreep visible-only body-only ext-plist
     (lambda () (set-auto-mode t))))
 
 ;;;###autoload
-(defun org-jekyll-export-to-html
+(defun org-blog-export-to-html
   (&optional async subtreep visible-only body-only ext-plist)
   "Export current buffer to a HTML file adding some YAML front matter."
   (interactive)
@@ -249,11 +261,7 @@ replacing the heading date."
          ;; (file (org-export-output-file-name extension subtreep))
 	(org-export-coding-system org-html-coding-system)
 	file props heading time)
-    ;; add the time stamp prefix required by jekyll to the filename
-    ;; (setq file (concat
-    ;; 		(file-name-directory file)
-    ;; 		(format-time-string "%Y-%m-%d-" (current-time))
-    ;; 		(file-name-nondirectory file)))
+
     (if subtreep
 	(progn (setq heading (org-get-heading 't 't))
 	       (setq time (and (string-match org-ts-regexp-both heading)
@@ -264,7 +272,7 @@ replacing the heading date."
 			       org-ts-regexp-both "" heading)))
 	       (setq file (expand-file-name
 			   (concat heading extension)
-			   org-jekyll-path))
+			   org-blog-path))
 	       (setq tag (org-get-tags-at))
 	       (setq props (org-entry-properties))
 
@@ -272,15 +280,20 @@ replacing the heading date."
 		   (progn (plist-put ext-plist :tag tag)
 			  (plist-put ext-plist :date time))
 		 (setq ext-plist (plist-put ext-plist :tag tag))))
-      (setq file (org-export-output-file-name extension subtreep org-jekyll-path)))
-
-    (org-export-to-file 'jekyll file
+      (setq file (org-export-output-file-name extension subtreep org-blog-path)))
+    ;; add the time stamp prefix required by Jekyll to the filename
+    (if (string= org-blog-engine "jekyll")
+	(setq file (concat
+		    (file-name-directory file)
+		    (format-time-string "%Y-%m-%d-" (current-time))
+		    (file-name-nondirectory file))))
+    (org-export-to-file 'blog file
       async subtreep visible-only body-only ext-plist)))
 
 
 
 ;;;###autoload
-(defun org-jekyll-publish-to-html (plist filename pub-dir)
+(defun org-blog-publish-to-html (plist filename pub-dir)
   "Publish an org file to HTML with YAML front matter.
 
 FILENAME is the filename of the Org file to be published. PLIST
@@ -288,33 +301,33 @@ is the property list for the given project. PUB-DIR is the
 publishing directory.
 
 Return output file name."
-  (org-publish-org-to 'jekyll filename ".html" plist pub-dir))
+  (org-publish-org-to 'blog filename ".html" plist pub-dir))
 
 ;;;###autoload
-(defun org-jekyll-insert-export-options-template
+(defun org-blog-insert-export-options-template
   (&optional title date setupfile categories tags published layout)
-  "Insert a settings template for Jekyll exporter."
+  "Insert a settings template for Blog exporter."
   (interactive)
-  (let ((layout     (or layout org-jekyll-yaml-layout))
-        (published  (or published org-jekyll-yaml-published))
-	(tags       (or tags org-jekyll-yaml-tags))
-        (categories (or categories org-jekyll-yaml-categories)))
+  (let ((layout     (or layout org-blog-yaml-layout))
+        (published  (or published org-blog-yaml-published))
+	(tags       (or tags org-blog-yaml-tags))
+        (categories (or categories org-blog-yaml-categories)))
     (save-excursion
-      (insert (format (concat
-                       "#+TITLE: " title
-                       "\n#+DATE: " date
-                       "\n#+SETUPFILE: " setupfile
-                       "\n#+JEKYLL_LAYOUT: " layout
-                       "\n#+JEKYLL_CATEGORIES: " categories
-		       "\n#+JEKYLL_TAGS: " tags
-                       "\n#+JEKYLL_PUBLISHED: " published
-                       "\n\n* \n\n{{{more}}}"))))))
+      (insert (concat
+	       "#+TITLE: " title
+	       "\n#+DATE: " date
+	       "\n#+SETUPFILE: " setupfile
+	       "\n#+BLOG_LAYOUT: " layout
+	       "\n#+BLOG_CATEGORIES: " categories
+	       "\n#+BLOG_TAGS: " tags
+	       "\n#+BLOG_PUBLISHED: " published
+	       "\n\n* \n")))))
 
 
 ;;;###autoload
-(defun org-jekyll-export-to-blog ()
+(defun org-blog-export-to-blog ()
   (interactive)
-  (let ((properties (org-jekyll-property-list))
+  (let ((properties (org-blog-property-list))
 	position)
     (save-excursion
       (goto-char (point-min))
@@ -322,9 +335,9 @@ Return output file name."
 	(setq position (point))
 	(org-forward-heading-same-level 1)
 	;; (message "%d" (point))
-	(org-jekyll-export-headline properties)))))
+	(org-blog-export-headline properties)))))
 
 
-(provide 'ox-jekyll)
+(provide 'ox-blog)
 
-;;; ox-jekyll end here
+;;; ox-blog end here
